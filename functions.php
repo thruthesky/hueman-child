155c6ad49fb5dc1623957bc96399f91523cd82fd
thruthesky <thruthesky@gmail.com>
2016-03-03 22:36:46 +0900

    work for hueman child theme

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file functions.php
+ *
+ *
+ */
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/etc/font-awesome/css/font-awesome.min.css');
+    wp_enqueue_script( 'hueman-child', get_stylesheet_directory_uri() . '/js/hueman-child.js' );
+}
