3dcac285d93c2f7cf662fe38f1f4ddc312e0b75a
thruthesky <thruthesky@gmail.com>
2016-03-17 13:13:38 +0900

    work

--- functions.php
+++ functions.php
@@ -4,6 +4,9 @@
  *
  *
  */
+
+include get_stylesheet_directory() . '/wp-include/library.php';
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
