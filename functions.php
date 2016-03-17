380771b87e1d87197e29c74a54dd4cfbac84f08f
thruthesky <thruthesky@gmail.com>
2016-03-17 12:57:35 +0900

    adding wp-incude/library.php

--- functions.php
+++ functions.php
@@ -12,9 +12,3 @@ function theme_enqueue_styles() {
     wp_enqueue_script( 'hueman-child', get_stylesheet_directory_uri() . '/js/hueman-child.js' );
 }
 add_action('after_setup_theme', 'remove_admin_bar');
-function remove_admin_bar() {
-	if (!current_user_can('administrator') && !is_admin()) {
-		show_admin_bar(false);
-	}
-}
-
